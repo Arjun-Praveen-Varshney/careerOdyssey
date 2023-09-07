8af2feed01bd1169489452e99d71b6eea6aec990
unknown <arjun.varshney1423@gmail.com>
2023-09-07 13:25:33 +0530

    changed firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRSS3XQ0Y6O-9i8JOQ7CdBJxaC_5gY9HY",
-  authDomain: "careerodyssey-arjun.firebaseapp.com",
-  projectId: "careerodyssey-arjun",
-  storageBucket: "careerodyssey-arjun.appspot.com",
-  messagingSenderId: "1070639741253",
-  appId: "1:1070639741253:web:e0c20006a8952e9bc8a62c",
-  measurementId: "G-YSW77PT69C",
+  apiKey: "AIzaSyBeaaGInjybl4qURwC-y26VPnIprWYlpHw",
+  authDomain: "resumeanalyzer-394008.firebaseapp.com",
+  projectId: "resumeanalyzer-394008",
+  storageBucket: "resumeanalyzer-394008.appspot.com",
+  messagingSenderId: "709669872867",
+  appId: "1:709669872867:web:54ca436958d2e698c991d6",
+  measurementId: "G-K6VC4MY8XR",
 };
 
 export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
+export const storage = getStorage(app);
